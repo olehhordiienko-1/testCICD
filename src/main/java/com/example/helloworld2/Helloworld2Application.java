5824818f6f554db2cb964746064a04913c4097c5
olehhordiienko-1 <116458066+olehhordiienko-1@users.noreply.github.com>
2022-11-30 13:29:55 +0200

    Update Helloworld2Application.java

--- src/main/java/com/example/helloworld2/Helloworld2Application.java
+++ src/main/java/com/example/helloworld2/Helloworld2Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Helloworld2Application {
