30fafefdfe44d1eca15deced1a86a6cf490fafff
olehhordiienko-1 <116458066+olehhordiienko-1@users.noreply.github.com>
2022-11-30 18:23:24 +0200

    port

--- src/main/java/com/example/helloworld2/Helloworld2Application.java
+++ src/main/java/com/example/helloworld2/Helloworld2Application.java
@@ -2,12 +2,22 @@ package com.example.helloworld2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Helloworld2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Helloworld2Application.class, args);
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String index() {
+        return "Hello World " + LocalDateTime.now();
     }
 
 }
