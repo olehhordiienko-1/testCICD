7ab28b699e56a4130a77ce3a43ebc5d9ea9a3469
olehhordiienko-1 <116458066+olehhordiienko-1@users.noreply.github.com>
2022-11-30 13:28:00 +0200

    Update Helloworld2Application.java

--- src/main/java/com/example/helloworld2/Helloworld2Application.java
+++ src/main/java/com/example/helloworld2/Helloworld2Application.java
@@ -2,6 +2,8 @@ package com.example.helloworld2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class Helloworld2Application {
